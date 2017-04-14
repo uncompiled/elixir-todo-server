400bdd928f2f6eef4e7f166d448958e8858e9f77
uncompiled <chris@uncompiled.org>
2017-04-13 21:32:47 -0400

    Convert process registry to ETS table

--- README.md
+++ README.md
@@ -17,13 +17,14 @@ Starting Todo.DatabaseWorker 3
 Starting Todo.ServerSupervisor
 Starting Todo.Cache
 {:ok, #PID<0.119.0>}
-iex(2)> my_list = Todo.Cache.server_process("my_list")
-Starting Todo.Server for my_list
+iex(2)> my_list = Todo.Cache.server_process("my-list")
+Starting Todo.Server for my-list
 #PID<0.128.0>
-iex(3)> Todo.Server.entries(my_list, {2017, 03, 01})
+iex(3)> Todo.Server.entries(my_list, {2017, 04, 13})
 []
-iex(4)> Todo.Server.add_entry(my_list, %{date: {2017, 02, 27}, title: "Meeting"})
+iex(4)> Todo.Server.add_entry(my_list, %{date: {2017, 04, 13}, title: "Meeting"})
 :ok
-iex(5)> Todo.Server.entries(my_list, {2017, 02, 27})
-[%{date: {2017, 2, 27}, id: 1, title: "Meeting"}]
+iex(5)> Todo.Server.entries(my_list, {2017, 04, 13})
+[%{date: {2017, 4, 13}, id: 1, title: "Meeting"}]
+
 ```
--- lib/todo/process_registry.ex
+++ lib/todo/process_registry.ex
@@ -1,6 +1,5 @@
 defmodule Todo.ProcessRegistry do
   use GenServer
-  import Kernel, except: [send: 2]
 
   def start_link do
     IO.puts "Starting Todo.ProcessRegistry"
@@ -12,7 +11,10 @@ defmodule Todo.ProcessRegistry do
   end
 
   def whereis_name(key) do
-    GenServer.call(:process_registry, {:whereis_name, key})
+    case :ets.lookup(:process_registry, key) do
+      [{^key, pid}] -> pid
+      _ -> :undefined
+    end
   end
 
   def unregister_name(key) do
@@ -29,48 +31,30 @@ defmodule Todo.ProcessRegistry do
   end
 
   def init(_) do
-    {:ok, HashDict.new}
+    :ets.new(:process_registry, [:named_table, :protected, :set])
+    {:ok, nil}
   end
 
 
-  def handle_call({:register_name, key, pid}, _, process_registry) do
-    case HashDict.get(process_registry, key) do
-      nil ->
-        # Sets up a monitor to the registered process
-        Process.monitor(pid)
-        {:reply, :yes, HashDict.put(process_registry, key, pid)}
-      _ ->
-        {:reply, :no, process_registry}
+  def handle_call({:register_name, key, pid}, _, state) do
+    if whereis_name(key) != :undefined do
+      {:reply, :no, state}
+    else
+      Process.monitor(pid)
+      :ets.insert(:process_registry, {key, pid})
+      {:reply, :yes, state}
     end
   end
 
-  def handle_call({:whereis_name, key}, _, process_registry) do
-    {:reply, HashDict.get(process_registry, key, :undefined), process_registry}
+  def handle_call({:unregister_name, key}, _, state) do
+    :ets.delete(:process_registry, key)
+    {:reply, key, state}
   end
 
-  def handle_call({:unregister_name, key}, _, process_registry) do
-    {:reply, key, HashDict.delete(process_registry, key)}
-  end
-
-  def handle_info({:DOWN, _, :process, pid, _}, process_registry) do
-    {:noreply, deregister_pid(process_registry, pid)}
+  def handle_info({:DOWN, _, :process, terminated_pid, _}, state) do
+    :ets.match_delete(:process_registry, {:_, terminated_pid})
+    {:noreply, state}
   end
 
   def handle_info(_, state), do: {:noreply, state}
-
-
-  defp deregister_pid(process_registry, pid) do
-    # We'll walk through each {key, value} item, and delete those elements whose
-    # value is identical to the provided pid.
-    Enum.reduce(
-      process_registry,
-      process_registry,
-      fn
-        ({registered_alias, registered_process}, registry_acc) when registered_process == pid ->
-          HashDict.delete(registry_acc, registered_alias)
-
-        (_, registry_acc) -> registry_acc
-      end
-    )
-  end
 end
